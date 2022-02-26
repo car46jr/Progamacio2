ce5c52efa3181766022f59ad33243542c9200429
Carlos <cgonzalezr19@miumg.edu.gt>
2022-02-26 09:52:17 -0600

    Primer Aporte

---
+++ main.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+float Numero1 , Numero2, total;
+int elec;
+
+int main ()
+{
+cout << " Ingrese el numero de la operacion que desea realizar"<<endl;
+cout << " 1. Suma " << endl;
+cout << " 2. Resta " << endl;
+cout << " 3. Divison" << endl;
+cout << " 4. Multiplicacion " << endl;
+cin>>(elec);
+
+
+
+cout<<( " Ingrese Primer Numero " )<<endl;
+cin>>(Numero1); 
+
+cout<<( "Ingrese Segundo Numero" )<<endl;
+cin>>(Numero2);
+
+
+	
+
+return 0;
+}
+}
+
+
